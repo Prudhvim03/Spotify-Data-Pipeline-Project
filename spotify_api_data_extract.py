de1a9ab87f6e07a99be0e7824fb2e1c48225ec2d
Prudhvim03 <113105434+Prudhvim03@users.noreply.github.com>
2025-03-03 15:52:42 +0530

    spotify_api_data_extract_function.py

--- spotify_api_data_extract.py
+++ spotify_api_data_extract.py
@@ -1,9 +1,3 @@
-import json
-import os
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import boto3
-from datetime import datetime
 
 def lambda_handler(event, context):
     
@@ -12,22 +6,25 @@ def lambda_handler(event, context):
     
     client_credentials_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
     sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-    playlists = sp.user_playlists('spotify')
+    playlists = sp.user_playlists('spotify')import json
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import boto3
+from datetime import datetime
+
     
-    playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
+    playlist_link = "https://open.spotify.com/playlist/1YUVG3tWJL8bYRpNiRFd29"
     playlist_uri = playlist_link.split("/")[-1].split("?")[0]
     
     spotify_data = sp.playlist_tracks(playlist_uri)
-    
+    print(spotify_data)
     client = boto3.client('s3')
     
     filename = "spotify_raw_" +str(datetime.now())+ ".json"
     
     client.put_object(
-        Bucket = "spotify-etl-project-shweta",
+        Bucket = "spotifyetlp0",
         Key = "raw_data/to_processed/" + filename,
         Body = json.dumps(spotify_data)
         )
-        
-    
-    
